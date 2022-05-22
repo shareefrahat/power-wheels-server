e53272543f1fffc8bdb6fb87554b6b4eefd28f81
ShareefRahat <imr4h47@gmail.com>
2022-05-22 23:32:44 +0600

    first with with environment setup

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+// const jwt = require("jsonwebtoken");
+require("dotenv").config();
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//------------Middleware-----------\\
+
+app.use(express.json());
+app.use(cors());
+
+//-----------Node Server API----------\\
+
+app.get("/", (req, res) => {
+  res.send("Server: Node server in running with Express");
+});
+
+app.listen(port, () => {
+  console.log(`Server app listening on port ${port}`);
+});
