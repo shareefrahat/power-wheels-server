8494b0aa3e02505b7ef8632421d7f04d22bf7a9c
ShareefRahat <imr4h47@gmail.com>
2022-05-23 00:11:56 +0600

    mongodb data collection added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 // const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 // const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
@@ -11,6 +12,31 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+//-----------MongoDB API-------------\\
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ygn3v.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Database: MongDB is connected");
+
+    const productCollection = client.db("PowerWheels").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const products = await productCollection.find().toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 //-----------Node Server API----------\\
 
