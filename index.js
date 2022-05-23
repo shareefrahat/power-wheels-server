df6929da277f1bc124cd1e77ad6b791f652344ed
ShareefRahat <imr4h47@gmail.com>
2022-05-23 17:27:17 +0600

    users api added with jwt secret key

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 // const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
@@ -22,16 +22,56 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+//--------Verify JWT----------\\
+
+// function verifyJWT(req, res, next) {
+//   const authHeader = req?.headers?.authorization;
+//   if (!authHeader) {
+//     return res.status(401).send({ message: "Unauthorized access" });
+//   }
+//   const token = authHeader.split(" ")[1];
+
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+//     if (err) {
+//       return res.status(403).send({ message: "Forbidden access" });
+//     }
+//     req.decoded = decoded;
+//     next();
+//   });
+// }
+
 async function run() {
   try {
     await client.connect();
     console.log("Database: MongDB is connected");
 
     const productCollection = client.db("PowerWheels").collection("products");
+    const userCollection = client.db("PowerWheels").collection("users");
 
+    //--------GET All PRODUCTS-------\\
     app.get("/products", async (req, res) => {
       const products = await productCollection.find().toArray();
       res.send(products);
+    });
+
+    //--------Save User in DB and Create JWT when SignUp---------\\
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params?.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1d" }
+      );
+      res.send({ result, token });
     });
   } finally {
   }
