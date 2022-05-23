0c3b13998c0a9558b1371fae6c01c1a290fbc092
ShareefRahat <imr4h47@gmail.com>
2022-05-23 17:30:50 +0600

    verify jwt function is added

--- index.js
+++ index.js
@@ -24,21 +24,21 @@ const client = new MongoClient(uri, {
 
 //--------Verify JWT----------\\
 
-// function verifyJWT(req, res, next) {
-//   const authHeader = req?.headers?.authorization;
-//   if (!authHeader) {
-//     return res.status(401).send({ message: "Unauthorized access" });
-//   }
-//   const token = authHeader.split(" ")[1];
+function verifyJWT(req, res, next) {
+  const authHeader = req?.headers?.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
 
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-//     if (err) {
-//       return res.status(403).send({ message: "Forbidden access" });
-//     }
-//     req.decoded = decoded;
-//     next();
-//   });
-// }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 async function run() {
   try {
